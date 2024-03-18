e4299d00a7dd92752b4a396dce004e98ead3a5a1
narayanacharyuluchitroju <raghava.chitroju@gmail.com>
2024-03-18 11:22:22 -0400

    init

--- src/main/java/com/wright/postgrescrud/controller/EmployeeController.java
+++ src/main/java/com/wright/postgrescrud/controller/EmployeeController.java
@@ -3,10 +3,9 @@ package com.wright.postgrescrud.controller;
 import com.wright.postgrescrud.model.Employee;
 import com.wright.postgrescrud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
@@ -23,5 +22,27 @@ public class EmployeeController {
             .build();
 
         return employeeService.createEmployee(emp);
+    }
+
+    @GetMapping("/get/{id}")
+    public Employee getEmployee(@PathVariable Long id){
+        return employeeService.getEmployee(id);
+    }
+
+    @GetMapping("/getEmployees")
+    public List<Employee> getEmployees(){
+        return employeeService.getEmployees();
+    }
+
+    @PutMapping("/update/{id}")
+    public String updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        Employee emp = Employee.builder()
+            .id(id)
+            .name(employee.getName())
+            .email(employee.getEmail())
+            .jobtitle(employee.getJobtitle())
+            .build();
+
+        return employeeService.updateEmployee(emp);
     }
 }
--- src/main/java/com/wright/postgrescrud/service/EmployeeService.java
+++ src/main/java/com/wright/postgrescrud/service/EmployeeService.java
@@ -5,6 +5,8 @@ import com.wright.postgrescrud.repo.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService {
 
@@ -14,5 +16,23 @@ public class EmployeeService {
     public String createEmployee(Employee employee) {
         employeeRepository.save(employee);
         return "Employee created successfully";
+    }
+
+    public Employee getEmployee(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public String updateEmployee(Employee employee) {
+        Employee emp = employeeRepository.findById(employee.getId()).orElse(null);
+        assert emp != null;
+        emp.setName(employee.getName());
+        emp.setEmail(employee.getEmail());
+        emp.setJobtitle(employee.getJobtitle());
+        employeeRepository.save(emp);
+        return "Employee updated successfully";
     }
 }
